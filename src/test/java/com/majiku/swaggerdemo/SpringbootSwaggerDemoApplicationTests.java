a540c1462a159b120f1fa168b22fd08115b9cc92
vinhsu <vinhsu@gmail.com>
2020-04-07 15:51:04 -0700

    initial commit.

---
+++ src/test/java/com/majiku/swaggerdemo/SpringbootSwaggerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majiku.swaggerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSwaggerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
